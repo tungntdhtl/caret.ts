6cce371b05954155da9d2b1ae86241400f1813b3
sfeuerriegel <stefan.feuerriegel@is.uni-freiburg.de>
2016-10-11 11:59:29 +0200

    Fixed train.ts() data

--- R/train.R
+++ R/train.R
@@ -6,8 +6,10 @@
 #' @param ... Further arguments passed on to \code{\link[caret]{train}} for training.
 #' @return Trained model.
 #' @examples 
-#' library(forecast)
 #' library(caret)
+#' 
+#' library(forecast)
+#' data(WWWusage)
 #' 
 #' class(WWWusage)
 #' str(WWWusage)
@@ -16,9 +18,35 @@
 #' summary(arima)
 #' arimaorder(arima$finalModel) # order of best model
 #' RMSE(predict(arima, df), df) # in-sample RMSE
+#' 
+#' library(vars)
+#' data(Canada)
+#' 
+#' class(Canada)
+#' str(Canada)
+#' 
+#' arima <- train(x = Canada[, -2], y = Canada[, 2], 
+#'                method = auto_arima_model(), trControl = trainDirectFit())
+#' summary(arima)
+#' arimaorder(arima$finalModel) # order of best model
+#' RMSE(predict(arima, df), df) # in-sample RMSE
+#' 
+#' arima <- train(form = prod ~ ., data = Canada, 
+#'                method = auto_arima_model(), trControl = trainDirectFit())
+#' summary(arima)
+#' arimaorder(arima$finalModel) # order of best model
+#' RMSE(predict(arima, df), df) # in-sample RMSE
 #' @importFrom caret train
 #' @export
 train.ts <- function(x, ...) {
-  df <- data.frame(y = as.numeric(x))
-  return(caret::train(y ~ 1, df, ...))
+  x <- coredata(x)
+  
+  if ("y" %in% names(list(...)) && class(list(...)[["y"]]) == "ts") {
+    inputArgs <- list(...)
+    inputArgs[["y"]] <- coredata(inputArgs[["y"]])
+  } else { 
+    inputArgs <- list(...)  
+  }
+  
+  return(do.call(caret::train, c(list(x = x), inputArgs)))
 }
